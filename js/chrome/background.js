c06fd70e4e0da21c739e86e596808b77c7b6e2ff
Fernando Bevilacqua <dovyski@gmail.com>
2014-01-14 07:29:33 -0200

    Small fixes

--- js/chrome/background.js
+++ js/chrome/background.js
@@ -3,7 +3,7 @@
  */
 
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('../../index.html', {id:"CodebotWin", bounds: {width: 800, height: 500}}, function(win) {
+  chrome.app.window.create('../../index.html', {id:"CodebotWin", bounds: {width: 1200, height: 800}}, function(win) {
     win.contentWindow.launchData = launchData;
   });
 });
