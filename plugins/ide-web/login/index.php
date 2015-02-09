34c87d259d13cfa006512808e37050e6633fc16b
Fernando Bevilacqua <dovyski@gmail.com>
2015-02-09 09:26:52 -0200

    Remove .htaccess dependency in login page.

--- plugins/ide-web/login/index.php
+++ plugins/ide-web/login/index.php
@@ -7,23 +7,26 @@
  *
  */
 
-/**
- * Define paths
- */
+// Define paths
 define('CONF_FILE', dirname(__FILE__).'/'.'opauth.conf.php');
 define('OPAUTH_LIB_DIR', dirname(__FILE__).'/inc/lib/Opauth/');
 
-/**
-* Load config
-*/
+// Load config
 if (!file_exists(CONF_FILE)) {
 	trigger_error('Config file missing at '.CONF_FILE, E_USER_ERROR);
 	exit();
 }
 require CONF_FILE;
 
-/**
- * Instantiate Opauth with the loaded config
- */
-require OPAUTH_LIB_DIR.'Opauth.php';
-$aOpauth = new Opauth( $config );
+
+if(isset($_GET['action'])) {
+	// Fake the request_uri so Opauth will be happy and work like we want it to.
+	$_SERVER['REQUEST_URI'] = OAUTH_PATH . '/' . $_GET['action'];
+
+	// Instantiate Opauth with the loaded config
+	require OPAUTH_LIB_DIR.'Opauth.php';
+	$aOpauth = new Opauth( $config );
+
+} else {
+	echo 'Login screen';
+}
