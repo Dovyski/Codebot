acf6647134d6ffba1aaa7a1c853fa712294b3b15
Fernando Bevilacqua <dovyski@gmail.com>
2015-02-19 16:37:59 -0200

    Add local config file to ignore list.

--- plugins/ide-web/config.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-@define('DB_DSN',           'mysql:host=localhost;dbname=codebot');
-@define('DB_USER',			'root');
-@define('DB_PASSWORD',		'');
-
-@define('PROJECT_TEMPLATES_FOLDER',		'C:\wamp\www\tmp\templates\\');
-?>
