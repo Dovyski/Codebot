ad21b6de802c651e7dd5cf2363e2eccf710c5f6c
Fernando Bevilacqua <dovyski@gmail.com>
2015-05-15 16:26:32 -0300

    Add app.json
    The app.json file can be used to tweak Codebot to your needs during boot. At the moment, the only config entry available is "codebot.bootstrap", which tells what bootstrap script Codebot should load to start the party.

--- js/codebot.bootstrap.js
+++ js/codebot.bootstrap.js
@@ -29,22 +29,58 @@
  * node-web kit, etc) and load the corresponding bootstrapper.
  */
 
-var aIsNodeWebkit   = 'require' in window;
-var aIsChromeApp    = false; // TODO: check it correctly.
+function codebotBootstrap(theAppConfig) {
+    var aIsNodeWebkit, aIsChromeApp;
 
-$(function() {
-    $('#loading').remove();
+    console.log('CODEBOT [bootstrap] App configuration file (app.json):', theAppConfig);
 
-    if(aIsNodeWebkit) {
-        console.log('CODEBOT [bootstrap] Node-webkit app.');
-        $('body').append('<script type="text/javascript" src="./js/node-webkit/codebot.bootstrap.nw.js"></script>');
-
-    } else if(aIsChromeApp) {
-        console.log('CODEBOT [bootstrap] Chrome Packaged App.');
+    if(theAppConfig.bootstrap) {
+        // App config file tells us to use a custom-made bootstrapper.
+        // So be it!
+        console.log('CODEBOT [bootstrap] Firing up custom bootstrapper: ' + theAppConfig.bootstrap);
+        $('body').append('<script type="text/javascript" src="'+theAppConfig.bootstrap+'"></script>');
 
     } else {
-        // It's running in the browser.
-        console.log('CODEBOT [bootstrap] Browser app');
-        $('body').append('<script type="text/javascript" src="./js/web/codebot.bootstrap.web.js"></script>');
+        aIsNodeWebkit = 'require' in window;
+        aIsChromeApp  = false; // TODO: check it correctly.
+
+        if(aIsNodeWebkit) {
+            console.log('CODEBOT [bootstrap] Node-webkit app.');
+            $('body').append('<script type="text/javascript" src="./js/node-webkit/codebot.bootstrap.nw.js"></script>');
+
+        } else if(aIsChromeApp) {
+            console.log('CODEBOT [bootstrap] Chrome Packaged App.');
+
+        } else {
+            // It's running in the browser.
+            console.log('CODEBOT [bootstrap] Browser app');
+            $('body').append('<script type="text/javascript" src="./js/web/codebot.bootstrap.web.js"></script>');
+        }
     }
+
+    $('#loading').remove();
+}
+
+// Wait until the DOM is ready, then starts loading
+// the app configuration file.
+
+$(function() {
+    $.getJSON('./app.json')
+    .done(function(theData) {
+        if(theData.codebot) {
+            codebotBootstrap(theData.codebot);
+
+        } else {
+            console.error('Codebot app.json file has no "codebot" property. E.g. {"codebot": {...}}.');
+        }
+    })
+    .fail(function(theJqxhr, theTextStatus, theError) {
+        if(theJqxhr.status == 404) {
+            codebotBootstrap(null);
+
+        } else {
+            // Probably a syntax error.
+            console.error('Codebot app.json file has a sintax error:', theTextStatus, theError);
+        }
+    });
 });
